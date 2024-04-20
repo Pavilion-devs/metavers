46f9c3932389bf0a99c6d83fc2fbcd3ac3dff99d
Pavilion-devs <118903029+Pavilion-devs@users.noreply.github.com>
2024-04-20 18:31:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main.js
@@ -0,0 +1,13 @@
+// const hamburger = document.getElementById('hamburger');
+// const myDiv = document.querySelector('.navbar')
+
+// hamburger.addEventListener('click', (e) => {
+//     myDiv.classList.toggle('active');
+
+// });
+
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+})
